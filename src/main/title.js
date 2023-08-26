5439180e97119d70ebdfd3b981e6807c1b221690
Kamioda <aimegu2014@gmail.com>
2023-08-26 18:41:43 +0900

    言語データ読み取り部を言語ファイルローダーに置換

--- src/main/title.js
+++ src/main/title.js
@@ -1,8 +1,8 @@
 import lang from '../language.json'
+import { ReadLanguageData } from '../languageloader';
 
 export const SetTitle = (Language, Page) => {
-    const LangData = lang.contents.find(c => c.lang === Language);
-    if (LangData == null) return SetTitle(lang.default);
+    const LangData = ReadLanguageData(Language)
     const SubTitle = LangData.subtitles.find(s => s.page === Page);
     document.title = SubTitle == null ? LangData.title : (`${SubTitle.text} - ${LangData.title}`);
 };
