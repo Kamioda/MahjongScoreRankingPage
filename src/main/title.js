0d1f28fa80d49e33e546dc9fc460088f8e644d67
Kamioda <aimegu2014@gmail.com>
2023-08-27 14:58:38 +0900

    .titleを除去
    言語データの旧構造のままになっていた

--- src/main/title.js
+++ src/main/title.js
@@ -3,5 +3,5 @@ import { ReadLanguageData } from '../languageloader.js';
 export const SetTitle = (Language, Page) => {
     const LangData = ReadLanguageData(Language);
     const SubTitle = LangData.subtitles[Page];
-    document.title = SubTitle == null ? LangData.title : `${SubTitle.text} - ${LangData.title}`;
+    document.title = SubTitle == null ? LangData.title : `${SubTitle} - ${LangData.title}`;
 };
