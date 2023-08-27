d995dc144d96e870742da34cb553ccf242940f4f
Kamioda <aimegu2014@gmail.com>
2023-08-27 14:45:57 +0900

    pathから言語情報を取得するメソッドを追加

--- src/languageloader.js
+++ src/languageloader.js
@@ -1,4 +1,10 @@
-import lang from './language.json'
+import { useParams } from 'react-router-dom';
+import lang from './language.json';
+
+export const GetLanguageFromParameter = () => {
+    const urlParams = useParams();
+    return urlParams.language ?? lang.default;
+};
 
 export const ReadLanguageData = (Language) => {
     const data = lang.contents.find(c => c.lang === Language);
