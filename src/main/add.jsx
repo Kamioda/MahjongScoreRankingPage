590a993dfee471673ccab5702b66093a69b18a2f
Kamioda <aimegu2014@gmail.com>
2023-08-31 01:31:01 +0900

    run prettier

--- src/main/add.jsx
+++ src/main/add.jsx
@@ -4,56 +4,56 @@ import PageHeaderItem from '../header.jsx';
 import '../css/add.css';
 
 const createMainForm = (playerCount, Lang) => {
-    const [records, setRecords] = React.useState([...Array(playerCount)].map(() => ({ player_id: '', player_id_cache: '', invalid: false, score: 0 })));
+    const [records, setRecords] = React.useState(
+        [...Array(playerCount)].map(() => ({ player_id: '', player_id_cache: '', invalid: false, score: 0 }))
+    );
     const LangData = ReadLanguageData(Lang);
     const createPlayerIDInputForm = formCountIndex => {
         return React.createElement('dl', null, [
             React.createElement('dt', null, LangData.content.add.caption.player_id),
-            React.createElement(
-                'dd',
-                null,
-                [
-                    React.createElement(
-                        'section',
-                        null,
+            React.createElement('dd', null, [
+                React.createElement(
+                    'section',
+                    null,
+                    React.createElement('input', {
+                        type: 'text',
+                        pattern: '^([a-zA-Z0-9_]{1,})$',
+                        onChange: e => {
+                            const Data = records;
+                            if (!records[formCountIndex].invalid) Data[formCountIndex].player_id = e.target.value;
+                            records[formCountIndex].player_id_cache = e.target.value;
+                            setRecords(Data);
+                        },
+                    })
+                ),
+                React.createElement(
+                    'section',
+                    {
+                        className: 'checkbox_field',
+                    },
+                    [
                         React.createElement('input', {
-                            type: 'text',
-                            pattern: '^([a-zA-Z0-9_]{1,})$',
+                            type: 'checkbox',
+                            id: `$cpu_check_${formCountIndex}`,
                             onChange: e => {
                                 const Data = records;
-                                if (!records[formCountIndex].invalid) Data[formCountIndex].player_id = e.target.value;
-                                records[formCountIndex].player_id_cache = e.target.value;
+                                Data[formCountIndex].player_id = e.target.checked
+                                    ? 'CPU'
+                                    : Data[formCountIndex].player_id_cache;
+                                Data[formCountIndex].invalid = e.target.checked;
                                 setRecords(Data);
                             },
-                        })
-                    ),
-                    React.createElement(
-                        'section',
-                        {
-                            className: 'checkbox_field'
-                        },
-                        [
-                            React.createElement('input', {
-                                type: 'checkbox',
-                                id: `$cpu_check_${formCountIndex}`,
-                                onChange: e => {
-                                    const Data = records;
-                                    Data[formCountIndex].player_id = e.target.checked ? 'CPU' : Data[formCountIndex].player_id_cache;
-                                    Data[formCountIndex].invalid = e.target.checked;
-                                    setRecords(Data);
-                                }
-                            }),
-                            React.createElement('label', 
-                                {
-                                    htmlFor: `$cpu_check_${formCountIndex}`,
-                                    
-                                },
-                                'CPU'
-                            )
-                        ]
-                    )
-                ]
-            ),
+                        }),
+                        React.createElement(
+                            'label',
+                            {
+                                htmlFor: `$cpu_check_${formCountIndex}`,
+                            },
+                            'CPU'
+                        ),
+                    ]
+                ),
+            ]),
         ]);
     };
     const createScoreInputForm = formCountIndex => {
